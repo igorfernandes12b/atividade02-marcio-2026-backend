b427dccbf061445a63c9814d4d5cf3feedf7f8c7
EA - Igor Fernandes Rodrigues <fernandes_igor@educadventista.org>
2026-02-23 17:53:53 -0300

    css mais bonito

--- listar.js
+++ listar.js
@@ -3,11 +3,13 @@ fetch('http://localhost:3333/produtos')
   .then(data => {
     const lista = document.getElementById('lista')
 
+    lista.innerHTML = ''
+
     data.forEach(produto => {
       lista.innerHTML += `
         <div class="card">
           <h3>${produto.nome}</h3>
-          <p><strong>Preço:</strong> R$ ${produto.preco}</p>
+          <p class="preco">R$ ${produto.preco}</p>
           <p>${produto.descricao}</p>
           <p><strong>Categoria:</strong> ${produto.categoria}</p>
           <p><strong>Tipo:</strong> ${produto.tipo}</p>
